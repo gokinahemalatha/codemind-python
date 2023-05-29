df508fcae7c047af640688b1ea4bb6eb0e4a67e6
ghemalatha <99868864+gokinahemalatha@users.noreply.github.com>
2023-05-29 12:22:37 +0530

    n=int(input()) c=n for i in range(1,n+1):     for j in range(i,n+1):         print(chr(64+c),end=' ')     c-=1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
 n=int(input())
+c=n
 for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end='')
+    for j in range(i,n+1):
+        print(chr(64+c),end=' ')
+    c-=1
     print()
+    
